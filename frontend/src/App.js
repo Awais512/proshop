4653041d86676277898c4b4cbd4e250733ff5cc0
awais512 <raza.awais25@gmail.com>
2020-11-01 20:35:33 +0500

    Frontend Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import { Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };

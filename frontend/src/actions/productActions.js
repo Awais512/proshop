3cb407dfe1fd49afc71ffd23121f021d1e46e438
awais512 <raza.awais25@gmail.com>
2020-11-02 21:35:33 +0500

    Products Details Actions and Reducers

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_REQUEST_FAILED,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_REQUEST_FAILED,
 } from '../constants/productConstants';
 import Axios from 'axios';
 
@@ -13,6 +16,22 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_REQUEST_FAILED,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await Axios.get(`/api/products/${id}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST_FAILED,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message

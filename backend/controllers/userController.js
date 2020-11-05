601ad77d60c1659958a89b4c90163d6f2ff3f8ea
awais512 <raza.awais25@gmail.com>
2020-11-05 16:13:33 +0500

    Registering users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,4 +23,51 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//@desc     Register a new User
+//@route    POST api/users
+//@access   Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already Exist');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
+});
+
+//@desc     Get User Profile
+//@route    GET api/users/profile
+//@access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    return res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not Found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };

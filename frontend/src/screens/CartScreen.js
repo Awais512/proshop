5ce2d4962ead6a0066c0689fbace5af2d46476c3
awais512 <raza.awais25@gmail.com>
2020-11-05 11:48:58 +0500

    Removing items from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -30,7 +30,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartItems = (id) => {
-    console.log('Remove');
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {

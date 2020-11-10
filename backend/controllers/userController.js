3cc149b34a6211fca2b277d57ca58df8d9400f6a
awais512 <raza.awais25@gmail.com>
2020-11-10 18:27:19 +0500

    Get User By ID and Update User By ID Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -119,6 +119,44 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc     Get User by ID
+//@route    GET api/users/:id
+//@access   Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+//@desc     Update User Profile by Admin
+//@route    PUT api/users/users/:id
+//@access   Private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updatedUser = await user.save();
+    return res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not Found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -126,4 +164,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };

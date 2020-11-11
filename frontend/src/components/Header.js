fd91a3654bb02838f1d6a85df08e68371c62a944
awais512 <raza.awais25@gmail.com>
2020-11-11 16:26:43 +0500

    Product List Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
                   <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to='admin/productlist'>
+                  <LinkContainer to='/admin/productlist'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
 

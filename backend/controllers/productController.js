80d511792f9ff8cf64731c94205c851174da9b47
awais512 <raza.awais25@gmail.com>
2020-11-13 20:50:41 +0500

    Top Rated Products Carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -141,6 +141,14 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc     Get top rated Products
+//@route    GET api/products/top
+//@access   Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.json(products);
+});
+
 export {
   getProducts,
   getProduct,
@@ -148,4 +156,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
